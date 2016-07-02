43599d01770cef024e1ac1b55212ff3ccf90100a
dev-neko <pi3kp85@gmail.com>
2016-07-02 21:21:11 +0900

    Add '全部できること'

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe '四則演算' do
   it '10 - 1 should be 9' do
     expect(10 - 1).to eq 9
   end
+
+  it '全部できること' do
+    expect(1 + 2).to eq 3
+    expect(10 - 1).to eq 9
+    expect(4 * 8).to eq 32
+    expect(40 / 5).to eq 8
+  end
 end
